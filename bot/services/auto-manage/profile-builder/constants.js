e3625542fc47a1276164e01fa2ee3c01d285f95e
phong <phongnhhe163436@fpt.edu.vn>
2026-06-05 20:18:21 +0700

    Split bible profile builder modules

---
+++ bot/services/auto-manage/profile-builder/constants.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const MIN_PROFILE_DURATION_MS = 3 * 60 * 1000;
+const BIBLE_PROFILE_SOURCE = "bible";
+const ENTRY_SEP = "\x1f";
+
+module.exports = {
+  BIBLE_PROFILE_SOURCE,
+  ENTRY_SEP,
+  MIN_PROFILE_DURATION_MS,
+};
